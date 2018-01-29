e592d53a8f019a45823b80b2345f6bc3fbafd256
nooobguest <34414523+nooobguest@users.noreply.github.com>
2018-01-29 17:01:33 +0800

    Update tnwz.py

--- tnwz.py
+++ tnwz.py
@@ -11,24 +11,24 @@ def find_answer(j,quiz):
     cursor.execute('SELECT answer FROM a WHERE quiz="%s"'%quiz)
     result = cursor.fetchall()
     if result:
-        webbrowser.open('http://www.baidu.com/s?ie=UTF-8&wd='+quiz)
+        webbrowser.open('http://www.baidu.com/s?ie=UTF-8&wd='+quiz)                       #本地数据库中没找到问题则百度
         ans = {}
         while True:
-            f = open('C:\\Users\\john\\Desktop\\choose.txt','r',encoding = 'utf-8')
+            f = open('C:\\Users\\john\\Desktop\\choose.txt','r',encoding = 'utf-8')     #读取答完题之后，答案的数据
             try:
                 ans = json.loads(f.read())
             
-                if ans['data']['num'] == j['data']['num']:
+                if ans['data']['num'] == j['data']['num']:      #判断当前答案题号是否跟问题题号一致，因为答案比问题晚出来
                     print('num',ans['data']['num'])
                     break;
             except:
                 pass
             time.sleep(0.3)
-        answer = j['data']['options'][ans['data']['answer']-1]
+        answer = j['data']['options'][ans['data']['answer']-1]  #获取答案
         print(answer)
-        cursor.execute('INSERT INTO a (quiz, answer) VALUES("%s", "%s")'%(quiz,answer))
+        cursor.execute('INSERT INTO a (quiz, answer) VALUES("%s", "%s")'%(quiz,answer))#将问题，答案存入数据库
     else:
-        print(result)
+        print(result)               #如果本地数据库中有该问题则直接输出答案
     cursor.close()
     connect.close()
 def main():
@@ -39,10 +39,10 @@ def main():
         try:
             f = open('C:\\Users\\john\\Desktop\\response_body.txt','r',encoding = 'utf-8')
             
-            j = json.loads(f.read())
+            j = json.loads(f.read())        #把保存下来的数据转成字典
             if 'quiz' in j['data']:
                 quiz = j['data']['quiz']
-                if quiz == quiz_backup:
+                if quiz == quiz_backup:     #如果相等说明下一题还没来
                     pass
                 else:
                     print(quiz)
